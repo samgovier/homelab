3ea37265bb69c1c499acb396d59635cb183025ff
Sam Govier <govier@eodops.com>
2022-07-09 20:44:35 -0500

    create terraform files

---
+++ azurelab/tf/aci/main.tf
@@ -0,0 +1,12 @@
+# Creating a ACI instance module
+
+# I need:
+# Resource Group
+# Name
+# Region
+# Image Source
+# Size
+# Public or Private...? I think private, and then VPN in
+# Env Variables
+# Command Override if needed
+# TAGS
---
+++ azurelab/tf/main.tf
@@ -0,0 +1,12 @@
+terraform {
+    required_version = ">= 1.2"
+    required_providers {
+        azurerm = {
+            version = "3.13"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
